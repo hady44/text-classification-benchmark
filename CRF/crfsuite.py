eb3452e5ebb990ba774d811d049a666786856273
hady <hadyyasser23@gmail.com>
2018-06-28 18:57:49 +0200

    feat: use wnut17

--- CRF/crfsuite.py
+++ CRF/crfsuite.py
@@ -1,5 +1,6 @@
 import nltk
 import sklearn_crfsuite
+from sklearn_crfsuite.metrics import flat_classification_report
 from nltk import word_tokenize, pos_tag, ne_chunk
 from sklearn.externals import joblib
 from sklearn.metrics import  f1_score
@@ -8,55 +9,14 @@ from sklearn.preprocessing import MultiLabelBinarizer
 from	sklearn.base	import	clone
 from nltk.chunk import conlltags2tree, tree2conlltags
 from sklearn.metrics import make_scorer, accuracy_score, precision_score, recall_score, f1_score
+import definitions
 
-import re
-# train_sents = list(nltk.corpus.conll2002.iob_sents('eng.train'))
-# test_sents = list(nltk.corpus.conll2002.iob_sents('eng_new.testb'))
 train_sents = []
+train_sents_new = []
 test_sents = []
-
-with open('/home/hady/nltk_data/corpora/conll2002/eng_new.train','rb') as tsvin, open('new.csv', 'wb') as csvout:
-    sent = []
-    # try:
-    for word in tsvin:
-        word = word.split(" ")
-        word = [w.replace("\n", "") for w in word]
-        word = [w.replace("\r", "") for w in word]
-
-
-        # splitted = [str.strip(w) for w in word]
-        # print word
-        # print(splitted)
-        if word[0] == '':
-            train_sents.append(sent)
-            sent = []
-        else:
-            # if len(word[0].split(" ")) > 1:
-                # print word[0].split(" ")
-            temp = (word[0], word[1], word[2])
-            sent.append(temp)
-
-
-with open('/home/hady/nltk_data/corpora/conll2002/eng_new.testb','rb') as tsvin, open('new.csv', 'wb') as csvout:
-    sent = []
-    # try:
-    for word in tsvin:
-        word = word.split(" ")
-        word = [w.replace("\n", "") for w in word]
-        word = [w.replace("\r", "") for w in word]
-
-
-        # splitted = [str.strip(w) for w in word]
-        # print word
-        # print(splitted)
-        if word[0] == '':
-            test_sents.append(sent)
-            sent = []
-        else:
-            # if len(word[0].split(" ")) > 1:
-                # print word[0].split(" ")
-            temp = (word[0], word[1], word[2])
-            sent.append(temp)
+test_sents_new = []
+y_train = []
+y_test = []
 
 tf_idf_clone_1 = joblib.load('../one-hot-classifiers/tf-idf+svm_1.pkl')
 tf_idf_clone_2 = joblib.load('../one-hot-classifiers/tf-idf+svm_2.pkl')
@@ -170,58 +130,7 @@ def sent2labels(sent):
 def sent2tokens(sent):
     return [token for token, postag, label in sent]
 
-print "start"
-X_train = [sent2features(s) for s in train_sents]
-y_train = [sent2labels(s) for s in train_sents]
-# print(y_train)
-# #
-X_test = [sent2features(s) for s in test_sents]
-y_test = [sent2labels(s) for s in test_sents]
-# print "done with 1"
-#
-X_train_new = [sent2features_new(s) for s in train_sents]
-
-crf = sklearn_crfsuite.CRF(
-    algorithm='lbfgs',
-    c1=0.1,
-    c2=0.1,
-    max_iterations=20,
-    all_possible_transitions=False,
-)
-
-crf_new = sklearn_crfsuite.CRF(
-    algorithm='lbfgs',
-    c1=0.1,
-    c2=0.1,
-    max_iterations=20,
-    all_possible_transitions=False,
-)
-#
-crf.fit(X_train, y_train)
-crf_new.fit(X_train_new, y_train)
-
-# joblib.dump(crf, 'crf-suite-old.pkl', compress=9)
-joblib.dump(crf_new, 'crf-suite-new.pkl', compress=9)
-
-ner_new = joblib.load('crf-suite-new.pkl')
-ner_old = joblib.load('crf-suite-old.pkl')
-
-print (ner_new.score(X_test, y_test), "new_model")
-print (ner_old.score(X_test, y_test), "old_model")
-
-# print ner_old
-
-# CONST_WIKI_ALL = "/home/hady/PycharmProjects/text-classification-benchmarks/data/test_data/ritter_ner.tsv"
-CONST_WIKI_ALL = "../data/test_data/ritter_ner.tsv"
-
-# dataset = np.genfromtxt(CONST_WIKI_ALL, delimiter='\t', skip_header=1)
-
-import csv
-X_test_final = []
-y_test_final = []
-
-
-with open(CONST_WIKI_ALL,'rb') as tsvin, open('new.csv', 'wb') as csvout:
+with open('../data/test_data/WNUT/17/wnut17train.conll','rb') as tsvin, open('new.csv', 'wb') as csvout:
 
     sent = ""
     labels = []
@@ -236,8 +145,23 @@ with open(CONST_WIKI_ALL,'rb') as tsvin, open('new.csv', 'wb') as csvout:
             # splitted = [re.sub('[^A-Za-z0-9]+', '', w) for w in splitted]
             splitted = [w for w in splitted if len(w) >= 1]
             # print splitted
-            X_test_final.append(sent2features_new((tree2conlltags(ne_chunk(pos_tag(splitted))))))
-            y_test_final.append(labels)
+            features = sent2features_new((tree2conlltags(ne_chunk(pos_tag(splitted)))))
+            features[0].update({
+                # 'klass_1': tf_idf_clone_1.predict([sent])[0],
+                # 'klass': tf_idf_clone.predict([sent])[0],
+                # 'klass_2': tf_idf_clone_2.predict([sent])[0],
+                # 'klass_3': tf_idf_clone_3.predict([sent])[0]
+            })
+            # print features
+            train_sents.append(sent2features((tree2conlltags(ne_chunk(pos_tag(splitted))))))
+            train_sents_new.append(features)
+            # print "---------"
+            # print features
+            # print "---------"
+            # print(sent)
+            # exit(0)
+            y_train.append(labels)
+
             sent = ""
             labels = []
         else:
@@ -248,9 +172,7 @@ with open(CONST_WIKI_ALL,'rb') as tsvin, open('new.csv', 'wb') as csvout:
     # except:
     #     print
 
-print (ner_new.score(X_test_final, y_test_final), "new_model")
-
-with open(CONST_WIKI_ALL,'rb') as tsvin, open('new.csv', 'wb') as csvout:
+with open('../data/test_data/WNUT/17/emerging.test.annotated','rb') as tsvin, open('new.csv', 'wb') as csvout:
 
     sent = ""
     labels = []
@@ -265,8 +187,18 @@ with open(CONST_WIKI_ALL,'rb') as tsvin, open('new.csv', 'wb') as csvout:
             # splitted = [re.sub('[^A-Za-z0-9]+', '', w) for w in splitted]
             splitted = [w for w in splitted if len(w) >= 1]
             # print splitted
-            X_test_final.append(sent2features((tree2conlltags(ne_chunk(pos_tag(splitted))))))
-            y_test_final.append(labels)
+            features = sent2features_new((tree2conlltags(ne_chunk(pos_tag(splitted)))))
+            features[0].update({
+                # 'klass_1': tf_idf_clone_1.predict([sent])[0],
+                # 'klass': tf_idf_clone.predict([sent])[0],
+                # 'klass_2': tf_idf_clone_2.predict([sent])[0],
+                # 'klass_3': tf_idf_clone_3.predict([sent])[0]
+            })
+
+            test_sents.append(sent2features((tree2conlltags(ne_chunk(pos_tag(splitted))))))
+            test_sents_new.append(features)
+            y_test.append(labels)
+
             sent = ""
             labels = []
         else:
@@ -274,9 +206,70 @@ with open(CONST_WIKI_ALL,'rb') as tsvin, open('new.csv', 'wb') as csvout:
                 # print word[0].split(" ")
             sent = sent + " " + str.strip(word[0])
             labels.append(word[1])
+    # except:
+    #     print
 
-print (ner_old.score(X_test_final, y_test_final), "old_model")
-    # y = ner_old.predict(X_test_final)
-    # print y
-    # print y_test_final
+print "start"
+X_train = train_sents
+X_train_new = train_sents_new
 
+# print(y_train)
+# #
+X_test = test_sents
+X_test_new = test_sents_new
+
+print "done with 1"
+
+crf = sklearn_crfsuite.CRF(
+    algorithm='lbfgs',
+    c1=0.088,
+    c2=0.002,
+    max_iterations=100,
+    all_possible_transitions=True,
+)
+
+crf_new = sklearn_crfsuite.CRF(
+    algorithm='lbfgs',
+    c1=0.088,
+    c2=0.002,
+    max_iterations=100,
+    all_possible_transitions=True,
+)
+
+crf.fit(X_train, y_train)
+print X_train_new
+crf_new.fit(X_train_new, y_train)
+
+joblib.dump(crf, 'crf-suite-old.pkl', compress=9)
+joblib.dump(crf_new, 'crf-suite-new.pkl', compress=9)
+
+ner_new = joblib.load('crf-suite-new.pkl')
+ner_old = joblib.load('crf-suite-old.pkl')
+
+# print (ner_new.score(X_test_new, y_test), "new_model")
+# print (ner_old.score(X_test, y_test), "old_model")
+#TODO: loop and check in test set for org
+
+new_pred = ner_new.predict(X_test_new)
+old_pred = ner_old.predict(X_test)
+
+#TODO: move this into a method
+
+labels = list(ner_new.classes_)
+labels.remove('O')
+labels.remove('B-creative-work')
+labels.remove('I-creative-work')
+labels.remove('B-product')
+labels.remove('I-product')
+labels.remove('B-group')
+labels.remove('I-group')
+
+idx = 0
+for label in new_pred:
+    for tok in label:
+        if tok.find('corporation') !=  -1 :
+            print label
+
+print "-----------------------------------------"
+print(flat_classification_report(y_test, new_pred, labels=labels,digits=3))
+print(flat_classification_report(y_test, old_pred, labels=labels, digits=3))
